7ef7604ec78e320b7af3ecf6be5d71ec09ef6822
jaeseonNamgung <sunnamgung8@naver.com>
2024-04-13 13:30:20 +0800

    spring boot initial
    - spring boot = 3.2.4
    - java 17 version

---
+++ src/main/java/com/hit/joonggonara/JoonggonaraApplication.java
@@ -0,0 +1,13 @@
+package com.hit.joonggonara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoonggonaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoonggonaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hit/joonggonara/JoonggonaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hit.joonggonara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoonggonaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
